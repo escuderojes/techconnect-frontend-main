17345b5ce9d5bd73a0933e5aea56523ba5512306
AlejandroJC12 <alejandrohh414@gmail.com>
2024-06-11 22:41:20 -0500

    Actualización pag-main-reclu.component.ts

--- src/app/reclutador/pag-main-reclu/pag-main-reclu.component.ts
+++ src/app/reclutador/pag-main-reclu/pag-main-reclu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pag-main-reclu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pag-main-reclu.component.html',
   styleUrl: './pag-main-reclu.component.css'
 })
