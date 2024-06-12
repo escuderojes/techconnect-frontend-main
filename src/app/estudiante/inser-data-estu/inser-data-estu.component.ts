1ce9fff828d8699036451acd97fa8b86918e1604
AlejandroJC12 <alejandrohh414@gmail.com>
2024-06-11 19:03:56 -0500

    Añadir inser-data-estu y inser-skill-estu a seguimiento de Git

---
+++ src/app/estudiante/inser-data-estu/inser-data-estu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inser-data-estu',
+  standalone: true,
+  imports: [],
+  templateUrl: './inser-data-estu.component.html',
+  styleUrl: './inser-data-estu.component.css'
+})
+export class InserDataEstuComponent {
+
+}
