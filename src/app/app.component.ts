6e8f65277bc29913bddb822b243f44cd5cb54c24
ELMERJIMENEZ29 <eldagiar_29@hotmail.com>
2024-06-18 17:35:19 -0500

    App.Component Actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,16 @@ import { RegisterRecluComponent } from './auth/register-reclu/register-reclu.com
 import { PresentationComponent } from './home/presentation/presentation.component';
 import { PagMainEstuComponent } from './estudiante/pag-main-estu/pag-main-estu.component';
 import { ProfileEstuComponent } from './estudiante/profile-estu/profile-estu.component';
-import { ActuDataEstuComponent } from './estudiante/actu-data-estu/actu-data-estu.component';
 import { InserSkillEstuComponent } from './estudiante/inser-skill-estu/inser-skill-estu.component';
 import { InserDataEstuComponent } from './estudiante/inser-data-estu/inser-data-estu.component';
 import { JobOfferComponent } from './reclutador/job-offer/job-offer.component';
-
-import { PagMainEstuComponent } from './estudiante/pag-main-estu/pag-main-estu.component';
 import { PageMainRecluComponent } from './reclutador/page-main-reclu/page-main-reclu.component';
-import { ProfileEstuComponent } from './estudiante/profile-estu/profile-estu.component';
-import { InserSkillEstuComponent } from './estudiante/inser-skill-estu/inser-skill-estu.component';
-import { InserDataEstuComponent } from './estudiante/inser-data-estu/inser-data-estu.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterEstuComponent, RegisterRecluComponent, PresentationComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterEstuComponent, RegisterRecluComponent, PresentationComponent, PagMainEstuComponent, PageMainRecluComponent, ProfileEstuComponent, InserSkillEstuComponent, InserDataEstuComponent, JobOfferComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
